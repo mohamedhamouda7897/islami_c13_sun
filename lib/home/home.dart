d999f3552f29c922d5cc452c0dbe0131ddb12c34
mohamedhamouda7897 <mohamedhamouda.mahmoud@gmail.com>
2024-12-11 20:38:17 +0200

    quran tab finished

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:islami_c13_sunday/home/bottom_nav_bar.dart';
 import 'package:islami_c13_sunday/home/tabs/ahadeth_tab.dart';
 import 'package:islami_c13_sunday/home/tabs/date_tab.dart';
-import 'package:islami_c13_sunday/home/tabs/quran_tab.dart';
+import 'package:islami_c13_sunday/home/tabs/quranTab/quran_tab.dart';
 import 'package:islami_c13_sunday/home/tabs/radio_tab.dart';
 import 'package:islami_c13_sunday/home/tabs/sebha_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "Home";
 
-  HomeScreen({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
@@ -27,51 +28,21 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        bottomNavigationBar: BottomNavigationBar(
-            showUnselectedLabels: false,
-            backgroundColor: Color(0xFFE2BE7F),
-            showSelectedLabels: true,
-            currentIndex: currentIndex,
-            onTap: (value) {
-              currentIndex = value;
-              setState(() {});
-            },
-            selectedItemColor: Colors.white,
-            unselectedItemColor: Color(0xFF202020),
-            type: BottomNavigationBarType.fixed,
-            items: [
-              BottomNavigationBarItem(
-                  backgroundColor: Color(0xFFE2BE7F),
-                  icon: _buildNavItem("quran", 0),
-                  label: "Quran"),
-              BottomNavigationBarItem(
-                  backgroundColor: Color(0xFFE2BE7F),
-                  icon: _buildNavItem("ahadeth", 1),
-                  label: "Hadith"),
-              BottomNavigationBarItem(
-                  backgroundColor: Color(0xFFE2BE7F),
-                  icon: _buildNavItem("sebha", 2),
-                  label: "Tasbeeh"),
-              BottomNavigationBarItem(
-                  backgroundColor: Color(0xFFE2BE7F),
-                  icon: _buildNavItem("ic_radio", 3),
-                  label: "Radio"),
-              BottomNavigationBarItem(
-                  backgroundColor: Color(0xFFE2BE7F),
-                  icon: _buildNavItem("dates", 4),
-                  label: "Time"),
-            ]),
+        bottomNavigationBar: BottomNavBar(onChange: (index) {
+          currentIndex = index;
+          setState(() {});
+        }),
         body: tabs[currentIndex],
       ),
     );
   }
 
   List<Widget> tabs = [
-    QuranTab(),
-    AhadethTab(),
-    SebhaTab(),
-    RadioTab(),
-    DateTab()
+     QuranTab(),
+    const AhadethTab(),
+    const SebhaTab(),
+    const RadioTab(),
+    const DateTab()
   ];
 
   String getBackgroundImageName() {
@@ -88,21 +59,5 @@ class _HomeScreenState extends State<HomeScreen> {
       default:
         return "home_bg";
     }
-  }
-
-  Widget _buildNavItem(String imageName, int index) {
-    return currentIndex == index
-        ? Container(
-            padding: EdgeInsets.symmetric(vertical: 6, horizontal: 20),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(66),
-                color: Color(0x80202020)),
-            child: ImageIcon(
-              AssetImage("assets/images/$imageName.png"),
-            ),
-          )
-        : ImageIcon(
-            AssetImage("assets/images/$imageName.png"),
-          );
   }
 }
---
+++ lib/home/tabs/quranTab/quran_tab.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:islami_c13_sunday/home/tabs/quranTab/sura_item_horizontal.dart';
+import 'package:islami_c13_sunday/home/tabs/quranTab/sura_name_item.dart';
+import 'package:islami_c13_sunday/models/sura_model.dart';
+
+class QuranTab extends StatefulWidget {
+  QuranTab({super.key});
+
+  @override
+  State<QuranTab> createState() => _QuranTabState();
+}
+
+class _QuranTabState extends State<QuranTab> {
+  var searchController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    searchController.addListener(onSearch);
+  }
+
+  onSearch() {
+    SuraModel.searchResult.clear();
+    String text = searchController.text;
+    if (text.isNotEmpty) {
+      for (String data in SuraModel.suraNamesEn) {
+        if (data.toLowerCase().contains(text.toLowerCase())) {
+          SuraModel.searchResult.add(data);
+        }
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/onboarding_header.png"),
+          _searchItem(),
+          if (SuraModel.searchResult.isNotEmpty ||
+              searchController.text.isNotEmpty) ...[_suraNamesHorizontalList()],
+          _suraNamesVerticalList(),
+        ],
+      ),
+    );
+  }
+
+  Widget _suraNamesHorizontalList() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Most Recently",
+          style: GoogleFonts.elMessiri(
+              fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        SuraModel.searchResult.isNotEmpty
+            ? const SizedBox()
+            : SizedBox(
+                height: 150,
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => const SizedBox(
+                    width: 16,
+                  ),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return SuraItemHorizontal(
+                      model: SuraModel.getSuraModel(index),
+                    );
+                  },
+                  itemCount: SuraModel.listCounts,
+                ),
+              ),
+        const SizedBox(
+          height: 8,
+        ),
+      ],
+    );
+  }
+
+  Widget _suraNamesVerticalList() {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Suras List",
+            style: GoogleFonts.elMessiri(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Expanded(
+            child: ListView.separated(
+              separatorBuilder: (context, index) => const Divider(
+                endIndent: 40,
+                indent: 40,
+              ),
+              padding: EdgeInsets.zero,
+              itemBuilder: (context, index) {
+                return SuraNameItem(
+                    model: SuraModel.searchResult.isNotEmpty
+                        ? SuraModel.getSelectedSuraModel(index)
+                        : SuraModel.getSuraModel(index));
+              },
+              itemCount: SuraModel.searchResult.isNotEmpty
+                  ? SuraModel.searchResult.length
+                  : SuraModel.listCounts,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _searchItem() {
+    return Column(
+      children: [
+        TextField(
+          style: GoogleFonts.elMessiri(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          controller: searchController,
+          cursorColor: Colors.white,
+          decoration: InputDecoration(
+            hintText: "Sura Name",
+            prefixIcon: const ImageIcon(
+              AssetImage("assets/images/ic_pre_search.png"),
+              color: Color(0xFFE2BE7F),
+            ),
+            hintStyle: GoogleFonts.elMessiri(
+                color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+            border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide:
+                    const BorderSide(color: Color(0xFFE2BE7F), width: 3)),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(12),
+                borderSide:
+                    const BorderSide(color: Color(0xFFE2BE7F), width: 3)),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: const BorderSide(color: Color(0xFFE2BE7F), width: 3),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
